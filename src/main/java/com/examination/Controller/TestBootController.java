d6bdefb9329f0b4f311930958ac1aee35b91ea05
Tocy <zyvj@qq.com>
2019-04-04 10:40:17 +0800

    two

---
+++ src/main/java/com/examination/Controller/TestBootController.java
@@ -0,0 +1,18 @@
+package com.examination.Controller;
+
+import com.examination.entity.User;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/testboot")
+public class TestBootController {
+    @RequestMapping("getuser")
+    public User getUser() {
+        User user = new User();
+        user.setName("test");
+        return user;
+    }
+}
--- src/main/java/com/examination/ExaminationApplication.java
+++ src/main/java/com/examination/ExaminationApplication.java
@@ -1,8 +1,10 @@
 package com.examination;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ExaminationApplication {
